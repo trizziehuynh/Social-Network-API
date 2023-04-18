990eae0f508d26663bd624c9c3de79a068452f16
Trizzie Huynh <putmind2822@gmail.com>
2023-04-17 18:17:54 -0700

    created models and connection

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /.+\@.+\..+/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//Create a virtual called `friendCount` that retrieves
+// the length of the user's `friends` array field on query.
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+//Create the User model
+
+const User = model("user", userSchema);
+
+module.exports = User;
